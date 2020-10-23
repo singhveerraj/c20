e12fe6df1db62d53952a7d5fa981463c7e178b3a
singhveerraj <72805099+singhveerraj@users.noreply.github.com>
2020-10-23 19:30:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var fr,mr;
+
+function setup() {
+  createCanvas(1200,800);
+  fr = createSprite(600, 400, 50, 80);
+fr.shapeColor = "blue";
+
+mr = createSprite(400, 200, 80, 30);
+mr.shapeColor = "blue";
+
+
+}
+
+function draw() {
+  background(0,0,0);
+  mr.x = mouseX;
+  mr.y = mouseY;
+
+  if(mr.x - fr.x < fr.width/2 + mr.width/2 
+     && fr.x - mr.x < fr.width/2 + mr.width/2 
+      && mr.y - fr.y < fr.height/2 + mr.height/2 
+       && fr.y - mr.y < fr.height/2 + mr.height/2){
+
+      mr.shapeColor = "yellow";
+      fr.shapeColor = "yellow";
+
+   
+  }  
+
+   else {
+    mr.shapeColor = "blue";
+    fr.shapeColor = "blue";
+   }
+
+
+
+  drawSprites();
+}
